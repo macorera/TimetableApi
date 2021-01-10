d728247f0ab8ba50f8f31dba5a7fd4fa2a2dbde9
Ashwin Corera <ashwincorera@gmail.com>
2021-01-11 02:47:13 +0530

    MSTest project added

---
+++ TimetableApi.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableApi.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient testClient;
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            testClient = appFactory.CreateClient();
+        }
+
+        
+    }
+}
---
+++ TimetableApi.IntegrationTest/TimetableControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableApi.IntegrationTest
+{
+    public class TimetableControllerTest : IntegrationTest
+    {
+        public async Task GetTimetables_Without_Path()
+        { 
+        }
+    }
+}
